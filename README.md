1c6929487c71d2a7b38a0df40718a67ecc592503
Kanahiro <mediterranean1769@gmail.com>
2021-10-02 22:47:31 +0900

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# deno-mbtiles-server-sample
+
+1. copy MBTIles files into `tiles` directory.
+2. launch server
+
+```sh
+deno run --allow-net --allow-read --allow-write server.ts
+```
---
+++ server.ts
@@ -0,0 +1,36 @@
+import { listenAndServe } from 'https://deno.land/std@0.109.0/http/server.ts';
+import { DB } from 'https://deno.land/x/sqlite@v3.1.1/mod.ts';
+import { expandGlob } from 'https://deno.land/std@0.109.0/fs/expand_glob.ts';
+import { gunzip } from 'https://deno.land/x/compress@v0.4.1/mod.ts';
+
+// make connections to .MBTiles files in './tiles'
+const mbtilesFileNames = [];
+for await (const mbtiles of expandGlob('tiles/*.mbtiles')) {
+    mbtilesFileNames.push(mbtiles.name);
+}
+type ConnectionDict = { [key: string]: DB };
+const connections = mbtilesFileNames.reduce(
+    (prev: ConnectionDict, cur: string) => ({
+        ...prev,
+        [cur.split('.')[0]]: new DB(`tiles/${cur}`),
+    }),
+    {} as ConnectionDict,
+);
+
+listenAndServe(':8000', (request) => {
+    const [tilename, z, x, y] = request.url.split('/').slice(-4);
+    const fy = Math.pow(2, Number(z)) - Number(y.split('.')[0]) - 1;
+
+    if (typeof connections[tilename] === 'undefined') {
+        return new Response('mbtiles is not exist', { status: 404 });
+    }
+
+    const pbf = connections[tilename].query(
+        `select tile_data from tiles where zoom_level=${z} and tile_column=${x} and tile_row=${fy};`,
+    );
+    if (pbf.length === 0) {
+        return new Response('tile is not exist', { status: 404 });
+    }
+
+    return new Response(gunzip(pbf[0][0] as Uint8Array));
+});
